917225f551857ec763726e8055db68e62597828b
Ranjit Patil <71892385+ranjit485@users.noreply.github.com>
2025-01-24 15:53:38 +0530

    Update gitprofile.tsx theme changer removed

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -197,6 +197,7 @@ const GitProfile = ({ config }: { config: Config }) => {
             <div className={`p-4 lg:p-10 min-h-full ${BG_COLOR}`}>
               <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 rounded-box">
                 <div className="col-span-1">
+                  <div className="grid grid-cols-1 gap-6">
                     <AvatarCard
                       profile={profile}
                       loading={loading}
